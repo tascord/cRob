1aa53876f3f669f029a36a5bd39118391aa21015
Oscar Hill <theactivesquare@gmail.com>
2020-02-23 17:49:58 +1100

    Removed music commands to prepare for merge

--- src/services/commands/test.js
+++
@@ -1,8 +0,0 @@
-const sm = require('../serverManager');
-
-exports.run = (client, message, args, send, createEmbed, config, fs, Discord) => {
-
-    console.log(sm.getServersByModify(message.author.id));
-    //send(createEmbed("Bruh", [{title: "beans", data: "wazo"}]), 0);
-
-}
